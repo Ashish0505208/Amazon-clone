47ea68e2de73bf9375d5212e9752bacb4bcd3ffb
Ashish0505208 <145243260+Ashish0505208@users.noreply.github.com>
2025-01-07 20:44:03 +0530

    split addtocart and moved addtocart into cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,5 @@
-import {cart} from '../data/cart.js';
+import {cart,addToCart} from '../data/cart.js';
+import {products} from '../data/products.js';
 
 let productshtml='';
 products.forEach((product) =>{
@@ -54,32 +55,23 @@ products.forEach((product) =>{
 
 document.querySelector('.js-products-grid').innerHTML=productshtml;
 
+
+
+function updatecartquantity(){
+        let cartQuantity=0;
+
+        cart.forEach((cartitem)=>{
+            cartQuantity+=cartitem.quantity;
+        });
+        document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
+}
+
 document.querySelectorAll('.js-add-to-cart').forEach((button) =>{
     button.addEventListener('click',()=>{
         const productId=button.dataset.productId;
-
-        let matchingItem;
-
-        cart.forEach((item)=>{
-            if (productId===item.productId){
-                matchingItem=item;
-            }
-        });
-
-        if(matchingItem){
-            matchingItem.quantity+=1;
-        }
-        else{cart.push({
-            productId: productId,
-            quantity: 1
-        });}
-
-        let cartQuantity=0;
-
-        cart.forEach((item)=>{
-            cartQuantity+=item.quantity;
-        });
-        document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
+        addToCart(productId);
+        updatecartquantity();
+        
     })
 })
 
