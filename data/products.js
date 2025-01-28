0632cc1e6fe13358f2d8d9cf378b59be9df185ef
Ashish0505208 <145243260+Ashish0505208@users.noreply.github.com>
2025-01-28 19:33:55 +0530

    loading products from backend

--- data/products.js
+++ data/products.js
@@ -89,6 +89,26 @@ const object3 = {
 object3.method();
 */
 
+
+export let products=[];
+
+export function loadproducts(fun){
+  const xhr=new XMLHttpRequest();
+  xhr.addEventListener('load', () =>{
+    products=JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+    fun();
+  });
+  xhr.open('GET','https://supersimplebackend.dev/products');
+  xhr.send();
+  
+}
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -754,3 +774,4 @@ export const products = [
   }
   return new Product(productDetails);
 });
+*/
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,6 +1,10 @@
 import {cart, addToCart} from '../data/cart.js';
-import {products} from '../data/products.js';
+import {products,loadproducts} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
+
+loadproducts(renderProductsGrid);
+
+function renderProductsGrid(){
 
 let productsHTML = '';
 
@@ -81,3 +85,4 @@ document.querySelectorAll('.js-add-to-cart')
       updateCartQuantity();
     });
   });
+}
