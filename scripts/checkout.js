b80bae6b54987cf4bf4d4fddd2aaefbded91aeec
Ashish0505208 <145243260+Ashish0505208@users.noreply.github.com>
2025-02-01 02:58:55 +0530

    promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,9 +1,44 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import { loadproducts } from '../data/products.js';
+import { loadcart } from '../data/cart.js';
 // import '../data/backend-practice.js';
 // import '../data/cart-class.js';
-loadproducts(() =>{
+
+Promise.all([
+    new Promise((resolve) =>{
+        loadproducts(() =>{
+            resolve();
+        });
+    }),
+    new Promise ((resolve) =>{
+        loadcart(() =>{
+            resolve();
+        });
+    })
+
+]).then(() =>{
     renderOrderSummary();
     renderPaymentSummary();
-})
+});
+
+// new Promise((resolve) =>{
+//     loadproducts(() =>{
+//         resolve();
+//     });
+// }).then(() =>{
+//     return new Promise ((resolve) =>{
+//         loadcart(() =>{
+//             resolve();
+//         });
+//     });
+// }).then(() =>{
+//     renderOrderSummary();
+//     renderPaymentSummary();
+// });
+
+
+// loadproducts(() =>{
+//     renderOrderSummary();
+//     renderPaymentSummary();
+// })
